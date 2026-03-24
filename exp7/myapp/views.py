77fdd03bdd48848532617ef37da193e4521e91ff
Dertyuggg <malligapmtc@gmail.com>
2026-03-24 10:56:35 +0530

    success

--- exp7/myapp/views.py
+++ exp7/myapp/views.py
@@ -1,32 +1,8 @@
-# Ex.07 Design of Interactive Image Gallery
-## Date:
+from django.http import HttpResponse
 
-## AIM:
-To design a web application for an inteactive image gallery for a minimum five images with next and previous buttons.
-
-## DESIGN STEPS:
-
-### Step 1:
-Clone the github repository and create Django admin interface.
-
-### Step 2:
-Change settings.py file to allow request from all hosts.
-
-### Step 3:
-Use CSS for positioning and styling.
-
-### Step 4:
-Write JavaScript program for implementing interactivity.
-
-### Step 5:
-Validate the HTML and CSS code.
-
-### Step 6:
-Publish the website in the given URL.
-
-## PROGRAM:
-```
- <html>
+def image_gallery(request):
+    return HttpResponse("""
+        <html>
             <head>
                 <title>DHYANESHWAR S</title>
                 <style>
@@ -187,10 +163,4 @@ Publish the website in the given URL.
             </body>
         </html>
 
-```
-
-## OUTPUT:
-
-![alt text](exp7/exp7/3b185cfb-09a1-473f-b307-6d0035ed2218.png)
-## RESULT:
-The program for designing an interactive image gallery using HTML, CSS and JavaScript is executed successfully.
+    """)
